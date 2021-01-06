d72984538c04b8294837ae06856b45e75a83a7b7
Benoit Hage <hage.benoit@gmail.com>
2021-01-06 13:10:43 +0100

    Footer & Mapbox

--- routes/index.py
+++ routes/index.py
@@ -2,7 +2,7 @@ import sqlite3
 import traceback
 import sys
 
-from flask import Blueprint, render_template, redirect, session, request
+from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
 from application import getUserName, getUserPicture, getUserRole, login_required, confirmed_required
 
 # Set Blueprints
@@ -13,4 +13,34 @@ index = Blueprint('index', __name__,)
 @confirmed_required
 def indexFunction():
 
-    return render_template("index.html", name=getUserName(), picture=getUserPicture(), role=getUserRole())
+    # Force flash() to get the messages on the same page as the redirect.
+    get_flashed_messages()
+
+    # Query database for plants
+    try:
+
+        sqliteConnection = sqlite3.connect("database.db")
+        cursor = sqliteConnection.cursor()
+        
+        # Query database
+        cursor.execute("SELECT * FROM plants;")
+        record = cursor.fetchall()
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+    
+        print("Failed to read data from sqlite table", error)
+        print("Exception class is: ", error.__class__)
+        print("Exception is", error.args)
+
+        print('Printing detailed SQLite exception traceback: ')
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        print(traceback.format_exception(exc_type, exc_value, exc_tb))
+
+    finally:
+
+        if (sqliteConnection):
+            sqliteConnection.close()
+
+    return render_template("index.html", name=getUserName(), picture=getUserPicture(), role=getUserRole(), plants=record)
