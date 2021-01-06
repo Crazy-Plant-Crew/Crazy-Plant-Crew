295b993cc64488781b0b9f35e0009b42019f72c9
Benoit Hage <hage.benoit@gmail.com>
2021-01-06 20:52:30 +0100

    Message & Footer links

--- routes/message.py
+++ routes/message.py
@@ -1,9 +1,11 @@
 import sqlite3
 import traceback
 import sys
+import os
 
-from flask import Blueprint, render_template, redirect, session, request
-from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole
+from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
+from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, getUserEmail, sendMail
+from flask_mail import Message, Mail
 
 # Set Blueprints
 message = Blueprint('message', __name__,)
@@ -15,6 +17,15 @@ def messageFunction():
 
     if request.method == "POST":
 
-        print("message")
+        subject = "Message from " + getUserName()
+        body = "Username: " + getUserName() + "\nEmail: " + getUserEmail() + "\n\nMessage: " + request.form.get("body")
+        email = os.environ["EMAIL_SEND"] 
+
+        sendMail(subject, email, body)
+
+        flash("Message sent")
+        return redirect("/")
+
+    else:
     
-    return render_template("message.html", name=getUserName(), picture=getUserPicture(), role=getUserRole())
+        return render_template("message.html", name=getUserName(), picture=getUserPicture(), role=getUserRole())
