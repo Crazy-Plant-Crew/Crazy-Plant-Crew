bf3180b83c356ab90367f85a65e89692a8b796df
Benoit Hage <hage.benoit@gmail.com>
2021-01-08 18:06:50 +0100

    News done

--- routes/article.py
+++ routes/article.py
@@ -2,7 +2,7 @@ import sqlite3
 import traceback
 import sys
 
-from flask import Blueprint, render_template, redirect, session, request
+from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
 from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required
 
 # Set Blueprints
@@ -11,10 +11,43 @@ article = Blueprint('article', __name__,)
 @article.route("/article", methods=["GET", "POST"])
 @login_required
 @confirmed_required
-def articleFunction():    
+def articleFunction():
+
+    # Force flash() to get the messages on the same page as the redirect.
+    get_flashed_messages()  
+
 
     if request.method == "POST":
 
-        print("article")
+        return redirect("/")
+
+    else:
+
+        # Query database for news to display them
+        try:
+
+            sqliteConnection = sqlite3.connect("database.db")
+            cursor = sqliteConnection.cursor()
+            
+            # Query database
+            cursor.execute("SELECT * FROM news;")
+            communications = cursor.fetchall()
+
+            cursor.close()
+
+        except sqlite3.Error as error:
+        
+            print("Failed to read data from sqlite table", error)
+            print("Exception class is: ", error.__class__)
+            print("Exception is", error.args)
+
+            print('Printing detailed SQLite exception traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
+
+        finally:
+
+            if (sqliteConnection):
+                sqliteConnection.close() 
     
-    return render_template("article.html", name=getUserName(), picture=getUserPicture(), role=getUserRole())
+        return render_template("article.html", name=getUserName(), picture=getUserPicture(), role=getUserRole(), communications=communications)
--- routes/change.py
+++ routes/change.py
@@ -2,7 +2,7 @@ import sqlite3
 import traceback
 import sys
 
-from flask import Blueprint, render_template, redirect, session, request
+from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
 from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required
 
 # Set Blueprints
@@ -11,10 +11,51 @@ change = Blueprint('change', __name__,)
 @change.route("/change", methods=["GET", "POST"])
 @login_required
 @confirmed_required
+@role_required
 def changeFunction():    
+
+    # Force flash() to get the messages on the same page as the redirect.
+    get_flashed_messages()  
 
     if request.method == "POST":
 
-        print("change")
+        # Get variable
+        news_id = request.form.get("news_id")
+        title = request.form.get("title")
+        body = request.form.get("ckeditor")
+
+        # Update plant name, stock, price, description and show status into the table
+        try:
+
+            sqliteConnection = sqlite3.connect("database.db")
+            cursor = sqliteConnection.cursor()
+
+            cursor.execute("UPDATE news SET title=:title, body=:body WHERE id=:id;", {"title": title, "body": body, "id": news_id})
+            sqliteConnection.commit()
+
+            cursor.close()
+
+        except sqlite3.Error as error:
+        
+            print("Failed to read data from sqlite table", error)
+            print("Exception class is: ", error.__class__)
+            print("Exception is", error.args)
+
+            print('Printing detailed SQLite exception traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
+
+        finally:
+
+            if (sqliteConnection):
+                sqliteConnection.close()
+        
+        flash("News edited")
+        return redirect("/communication")
+
+    else:
+
+        # Get arguments from url_for in administration
+        thisNews = request.args.getlist("news")
     
-    return render_template("change.html", name=getUserName(), picture=getUserPicture(), role=getUserRole())
+        return render_template("change.html", name=getUserName(), picture=getUserPicture(), role=getUserRole(), news=thisNews)
