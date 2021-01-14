e29b865e0ff0834251f9570ea77a3cdff1025971
BenoitHage <hage.benoit@gmail.com>
2021-01-14 12:01:28 +0100

    Transaction route

--- build/lib/routes/logout.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, redirect, session
-
-# Set Blueprints
-logout = Blueprint('logout', __name__,)
-
-@logout.route("/logout")
-def logoutFunction():
-    session.clear()
-    return redirect("/signin")
