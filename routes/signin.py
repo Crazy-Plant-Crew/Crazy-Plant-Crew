cedac2c7c2ae17c57282c63deb10d87ff3fc9e7d
BenoitHage <hage.benoit@gmail.com>
2021-01-27 12:44:43 +0100

    SQLAlchemy

--- routes/signin.py
+++ routes/signin.py
@@ -18,8 +18,10 @@ def signinFunction():
     # Force flash() to get the messages on the same page as the redirect.
     get_flashed_messages()
 
+
     # Forget any user_id
     session.clear()
+
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
@@ -32,29 +34,38 @@ def signinFunction():
             flash("must provide username")
             return redirect("/signin")
 
+
         # Ensure username fits server-side
         if not re.search("^[a-zA-Z0-9]{2,20}$", username):
             flash("Invalid username")
             return redirect("/signin")
+
 
         # Ensure password was submitted
         if not password:
             flash("must provide password")
             return redirect("/signin")
 
+
         # Query database for username if already exists
         query = Users.query.filter_by(username=username).all()
+
+        print(len(query))
+        print(check_password_hash(query.hash, password))
+
 
         # Ensure username exists and password is correct
         if query is not None or not check_password_hash(query.hash, password):
             flash("invalid username and/or password")
             return redirect("/signin")
 
+
         # Remember which user has logged in
         session["user_id"] = query.id
 
 
         return redirect("/")
+
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
