cdcceedae93fca44a278aedd3d048e853a653369
BenoitHage <hage.benoit@gmail.com>
2021-01-27 19:03:20 +0100

    SQLAlchemy

--- routes/add.py
+++ routes/add.py
@@ -95,12 +95,13 @@ def addFunction():
 
 
         # Query database for plant if already exists so the last is selected as the last plant is the one just added
-        query = Plants.query.filter(name=name).all()
+        query = Plants.query.filter_by(name=name).all()
+        print(query)
 
-        if len(query) != 1:
+        if len(query) > 1:
             plant_id = query[-1].id
 
-        else:
+        elif len(query) == 1:
             plant_id = query.id
 
 
