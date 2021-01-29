5c17d5174b2f27898dce1472a4528fd5a404fe3c
BenoitHage <hage.benoit@gmail.com>
2021-01-29 13:01:52 +0100

    SQLAlchemy

--- routes/article.py
+++ routes/article.py
@@ -2,11 +2,13 @@ import traceback
 import sys
 
 from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
-from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db
+from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db, News
 from flask_sqlalchemy import SQLAlchemy
+
 
 # Set Blueprints
 article = Blueprint('article', __name__,)
+
 
 @article.route("/article", methods=["GET", "POST"])
 @login_required
@@ -17,15 +19,14 @@ def articleFunction():
     get_flashed_messages()  
 
 
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         return redirect("/")
 
     else:
 
-        # Query database for news to display them
-        record = db.engine.execute("SELECT * FROM News;")
-        communications = record.fetchall()
-
+        # Update database with email
+        news = News.query.all()
     
-        return render_template("article.html", name=getUserName(), picture=getUserPicture(), role=getUserRole(), communications=communications)
+        return render_template("article.html", name=getUserName(), picture=getUserPicture(), role=getUserRole(), news=news)
--- routes/create.py
+++ routes/create.py
@@ -2,13 +2,16 @@ import traceback
 import sys
 import html2text
 
+
 from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
-from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db
+from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db, News
 from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
 
+
 # Set Blueprints
 create = Blueprint('create', __name__,)
+
 
 @create.route("/create", methods=["GET", "POST"])
 @role_required
@@ -19,6 +22,8 @@ def createFunction():
     # Force flash() to get the messages on the same page as the redirect.
     get_flashed_messages()  
 
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         # Get variable
@@ -26,10 +31,13 @@ def createFunction():
         body = request.form.get("ckeditor")
 
         # Insert title and body into the table            
-        db.engine.execute("INSERT INTO News(title, body) VALUES (:title, :body)", {"title": title, "body": body})
+        db.session.add(News(title=title, body=body))
+        db.session.commit()
 
+        # Flash result & redirect
         flash("News created")
         return redirect("/communication")
+        
 
     else:
     
