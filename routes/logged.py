cbf606866c738307b8c8619295c3c73b8d15bad3
BenoitHage <hage.benoit@gmail.com>
2021-02-01 19:26:01 +0100

    Users page

--- routes/logged.py
+++ routes/logged.py
@@ -1,7 +1,7 @@
 import traceback
 import sys
 
-from flask import Blueprint, render_template, redirect, session, request
+from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
 from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db, Users
 from flask_sqlalchemy import SQLAlchemy
 
