f9c00ee591658cbd8c3d4f241262240ffde8ce91
BenoitHage <hage.benoit@gmail.com>
2021-02-03 19:02:10 +0100

    Users page

--- routes/logged.py
+++ routes/logged.py
@@ -34,7 +34,7 @@ def loggedFunction():
         demote = request.form.get("demote")
 
 
-        if request.form.remove == "remove":
+        if request.form.action == "remove":
 
             # Loop through the DB query
             index = 0
@@ -82,7 +82,7 @@ def loggedFunction():
 
 
 
-        if request.form.promote == "promote":
+        if request.form.action == "promote":
 
             # Loop through the DB query
             index = 0
@@ -119,7 +119,7 @@ def loggedFunction():
 
 
 
-        if request.form.demote == "demote":
+        if request.form.action == "demote":
 
             # Loop through the DB query
             index = 0
