d7245ef6e5525c71f4d494ab73e3279621ceba87
BenoitHage <hage.benoit@gmail.com>
2021-02-16 14:40:13 +0100

    Boxes logic

--- routes/addBox.py
+++ routes/addBox.py
@@ -1,5 +1,6 @@
 import traceback
 import sys
+import re
 
 from flask import Blueprint, render_template, redirect, session, request, get_flashed_messages, flash
 from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db, Boxes, getInputLength
