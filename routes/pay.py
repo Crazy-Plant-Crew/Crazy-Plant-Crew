6755360c7d2f8c6d664635e0d1e4030a32686bb5
BenoitHage <hage.benoit@gmail.com>
2021-02-17 11:31:04 +0100

    Pay logic

--- routes/pay.py
+++ routes/pay.py
@@ -1,8 +1,9 @@
 import traceback
 import sys
+import re
 
-from flask import Blueprint, render_template, redirect, session, request
-from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db
+from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
+from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, role_required, db, Boxes, Users, Plants, Baskets
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -15,9 +16,20 @@ pay = Blueprint('pay', __name__,)
 @confirmed_required
 def payFunction():    
 
+    # Force flash() to get the messages on the same page as the redirect.
+    get_flashed_messages()
+
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        print("pay")
+        # Get variable
+        street = request.form.get("street")
+        house = request.form.get("house")
+        zipcode = request.form.get("zipcode")
+        country = request.form.get("country")
+        additional = request.form.get("additional")
+        
 
     else:
     
