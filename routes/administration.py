cca997051426d5242dc08c9822142962aa444b20
BenoitHage <hage.benoit@gmail.com>
2021-02-25 10:45:03 +0100

    Pay logic

--- routes/administration.py
+++ routes/administration.py
@@ -63,7 +63,7 @@ def administrationFunction():
 
                     # Create a list with values of DB and append them
                     thisPlant = []
-                    thisPlant.extend([plants[index].id, plants[index].name, plants[index].stock, plants[index].price, plants[index].offer, plants[index].length, plants[index].width, plants[index].height, plants[index].weight, plants[index].picture, plants[index].description, plants[index].reduced, plants[index].show])
+                    thisPlant.extend([plants[index].id, plants[index].name, plants[index].stock, plants[index].price, plants[index].offer, plants[index].length, plants[index].width, plants[index].height, plants[index].weight, plants[index].picture, plants[index].description, plants[index].express, plants[index].reduced, plants[index].show])
 
                     return redirect(url_for("editPlant.editPlantFunction", plants=thisPlant))
 
