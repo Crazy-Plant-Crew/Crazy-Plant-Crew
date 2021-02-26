bbbc61fb55f28d59912fc7cc8cb2285619c224e0
BenoitHage <hage.benoit@gmail.com>
2021-02-26 15:42:17 +0100

    Pay logic

--- routes/confirmation.py
+++ routes/confirmation.py
@@ -22,7 +22,7 @@ def confirmationFunction():
 
     # Get variable
     user_id = session["user_id"]
-    express = request.args.getlist("express")
+    express = request.args.get("express")
     selection = Baskets.query.filter_by(user_id=user_id)
 
 
