4850eac24d3aaf3c706af917b1d981b4cb77b9bd
BenoitHage <hage.benoit@gmail.com>
2021-03-02 18:56:12 +0100

    Pay logic

--- routes/confirmation.py
+++ routes/confirmation.py
@@ -115,14 +115,13 @@ def confirmationFunction():
                     for package in packaging:
                         if int(plant[4]) < int(package[1]) and int(plant[5]) < int(package[2]) and int(plant[6]) < int(package[3]):
                             boxes.append(package)
-                            print(package)
                             plantTemp.remove(plant)
                             boxPick()
             else:
                 return
 
                 
-        
+        boxPick()
         print(boxes)
                 
                 
