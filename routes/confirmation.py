801810e0d8adb30dbdac467997258e7f7ff9e0a8
BenoitHage <hage.benoit@gmail.com>
2021-03-02 14:46:13 +0100

    Pay logic

--- routes/confirmation.py
+++ routes/confirmation.py
@@ -113,13 +113,9 @@ def confirmationFunction():
                 if plants[indexPlants][4] < packaging[indexPackaging][1] and plants[indexPlants][5] < packaging[indexPackaging][2] and plants[indexPlants][6] < packaging[indexPackaging][3]:
                     
                     boxes.append(packaging[indexPackaging])
+                    indexPlants += 1
                     indexPackaging = 0
 
-                else:
-
-                    indexPackaging += 1
-
-            indexPlants += 1
 
 
         print("PLANTS")
