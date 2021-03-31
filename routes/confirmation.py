719329b04c2d2977823ea87c63cda6bed4556e0a
BenoitHage <hage.benoit@gmail.com>
2021-03-31 12:11:17 +0200

    Pay logic

--- routes/confirmation.py
+++ routes/confirmation.py
@@ -327,6 +327,8 @@ def confirmationFunction():
                             masterLoop(thisCost)
                 
                 else:
+                    print("slaveLoop thisCost")
+                    print(thisCost)
                     return thisCost
 
 
